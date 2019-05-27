83e444520af3e8e93a353c8931c1436462dbfd47
dima <dimaidiothack@gmail.com>
2019-05-27 23:57:55 +0300

    Support routes

--- routes/support.js
+++ routes/support.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const supportController = require('../controllers/support');
+const express = require("express");
+const { check } = require("express-validator/check");
+const supportController = require("../controllers/support");
 
 const router = express.Router();
 
-router.get('/support',supportController.getSupport);
-router.post('/support',supportController.postSupport);
+router.get("/support", supportController.getSupport);
+router.post(
+  "/support",
+  [
+    check("userEmail")
+      .isEmail()
+      .withMessage("Please enter a valid email address"),
+    check("feedback")
+      .isLength({ min: 10 })
+      .withMessage("Feedback must contain at least 10 letters")
+  ],
+  supportController.postSupport
+);
 
 module.exports = router;
